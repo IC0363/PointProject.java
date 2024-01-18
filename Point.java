fd87e281e19eaadea4871979fd49cae8cac9b6ba
IC0363 <ichoi0221@gmail.com>
2024-01-18 13:06:23 -0500

    Point

--- Point.java
+++ Point.java
@@ -9,15 +9,34 @@ public class Point{
 
 
     }
-    public int manhattanDistance(Point other){
-        int xDist = Math.abs(x) - Math.abs(other.getX());
-        int yDist = Math.abs(y) - Math.abs(other.getY());
-
-        return xDist + yDist;
-
+    public int getX() {
+        return x;
     }
 
+    public int getY() {
+        return y;
+    }
+
+    public int manhattanDistance(Point other){
+        int xDist = Math.abs(x - other.getX());
+        int yDist = Math.abs(y - other.getY());
+    
+        return xDist + yDist;
+    }
+    
+
     public boolean isCollinear(Point p1, Point p2){
+
+        
+
+        double s1 = (double)(y - p1.getY()) / (x - p1.getX());
+        double s2 = (double)(y - p2.getY()) / (x - p2.getX());
+        if(x==p1.getX() && x==p2.getX()){
+            return true;
+        }
+       
+        return Math.abs(s1 - s2) <.01;
+
             
 
 
